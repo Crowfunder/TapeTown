1cae4d56675f8d8207ec788b1e9dab8a6d51a440
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2025-10-04 14:28:02 +0200

    Boilerplate for flask backend

---
+++ app/database/models.py
@@ -0,0 +1,26 @@
+import datetime
+import sqlalchemy as sa
+from sqlalchemy.orm import mapped_column, relationship
+from flask_sqlalchemy import SQLAlchemy
+
+# Here you can read about useful column types (Integer, String, DateTime, etc...):
+# https://docs.sqlalchemy.org/en/20/core/type_basics.html#generic-camelcase-types
+
+# Here you can read about relationships between models:
+# https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
+# https://stackoverflow.com/questions/3113885/difference-between-one-to-many-many-to-one-and-many-to-many
+
+# Here you can read about using models defined below to work
+# with the database (creating rows, selecting rows, deleting rows, etc...):
+# https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/queries/
+
+
+
+db = SQLAlchemy()
+from dataclasses import dataclass
+
+@dataclass
+class temp(db.Model):
+    __tablename__ = ''
+
+    Id = db.Column(db.Integer, primary_key=True)
